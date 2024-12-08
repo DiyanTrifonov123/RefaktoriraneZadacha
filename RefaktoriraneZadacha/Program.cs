8126e3d3a85cd9311f6650e23c3907741c133e58
DiyanTrifonov123 <diyantrifonov5@gmail.com>
2024-12-08 11:12:53 +0200

    First commit

---
+++ RefaktoriraneZadacha/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefaktoriraneZadacha
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            LinkedList<int> numbers = new LinkedList<int>();
+            numbers.Append(1);
+            numbers.Append(2);
+            numbers.Append(3);
+            numbers.Append(4);
+            numbers.Append(5);
+            var evenNumbers = SplitList(numbers, "even");
+            var oddNumbers = SplitList(numbers, "odd");
+            Console.WriteLine("Списък с четни числа:");
+            evenNumbers.PrintList();
+            Console.WriteLine("Списък с нечетни числа:");
+            oddNumbers.PrintList();
+        }
+    }
+}
